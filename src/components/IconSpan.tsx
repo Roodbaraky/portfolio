c3c9ac515fc98583274e08569388b4108d002bd1
roodbaraky <kooroshr@hotmail.com>
2024-07-16 21:27:07 +0100

    Changed conditional themeing for skills icons / text for contrast

--- src/components/IconSpan.tsx
+++ src/components/IconSpan.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function IconSpan({children}:any) {
   return (
-    <span className="shrink-0 rounded-lg bg-neutral text-secondary opacity-80 hover:opacity-80 transition-all delay-100 ease-in-out hover:scale-105  p-2 my-2 flex flex-col content-center justify-center w-24 max-w-24 text-center min-h-24">
+    <span className={`shrink-0 rounded-lg bg-base-content text-secondary-content opacity-80 hover:opacity-80 transition-all delay-100 ease-in-out hover:scale-105  p-2 my-2 flex flex-col content-center justify-center w-24 max-w-24 text-center min-h-24`}>
         {children}
     </span>
   )
