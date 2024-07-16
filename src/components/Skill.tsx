d05cd01bf3de9049197f47c1b7ce47cb8d824104
roodbaraky <kooroshr@hotmail.com>
2024-07-16 22:33:19 +0100

    Tweaked skills outline behaviour AGAIN

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -10,7 +10,7 @@ export default function Skill({ title, description, children }: SkillProps) {
     <div
       id={title.toLowerCase()}
       aria-label={`${title} skils section`}
-      className="outline outline-secondary-content hover:outline-secondary hover:outline-4 hover:scale-105  shadow hover:shadow-lg transition-all duration-500 ease-in-out rounded-2xl p-4 flex flex-col justify-between min-h-96 min-w-96 max-w-96"
+      className="outline outline-1 outline-base-content hover:outline-secondary hover:outline-4 hover:scale-105  shadow hover:shadow-lg transition-all duration-500 ease-in-out rounded-2xl p-4 flex flex-col justify-between min-h-96 min-w-96 max-w-96"
     >
       <div>
         <h3 className="text-2xl text-center ">{title}</h3>
